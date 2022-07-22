c465d113b282d94f8059f32dd09f42d504a93003
Anirudh Sundar <anirudh.sundar@gmail.com>
2022-07-22 14:49:17 +0530

    Formatting with black

--- src/autodl/autodl.py
+++ src/autodl/autodl.py
@@ -3,7 +3,6 @@ This is a short script to automatically download the
 latest releases of certain tools (which are listed in repo_names.json).
 """
 
-import argparse
 import json
 import os
 import re
@@ -36,35 +35,37 @@ def main():
         exclude = args.exclude.split(",")
 
     if include and exclude:
-        print('ERROR: Cannot use both "--include" and "--exclude" flags simultaneously. Please remove one of them')
+        print(
+            'ERROR: Cannot use both "--include" and "--exclude" flags simultaneously. Please remove one of them'
+        )
 
     paths_to_append = []
     urls = []
     files = []
 
     repo_names_path = str(utils.get_config_path())
-    tool_config = json.load(open(repo_names_path, 'r'))
+    tool_config = json.load(open(repo_names_path, "r"))
     for tool in tool_config:
-        tool_name = tool['name']
+        tool_name = tool["name"]
         if include and tool_name not in include:
             continue
         if exclude and tool_name in exclude:
             continue
         if args.list:
-            print(tool['name'])
+            print(tool["name"])
             continue
         else:
-            print('Preparing to download',tool['name'])
-        user = tool['user']
-        repo = tool['repo']
-        file_pattern = tool['file_pattern']
+            print("Preparing to download", tool["name"])
+        user = tool["user"]
+        repo = tool["repo"]
+        file_pattern = tool["file_pattern"]
 
-        if 'uncompress' in tool and tool['uncompress']:
-            uncompress_cmd = tool['uncompress_cmd']
-            uncompress_flags = ''
-            if 'uncompress_flags' in tool:
-                uncompress_flags = tool['uncompress_flags']
-                uncompress_flags = re.sub(r'\s+', ' ', uncompress_flags.strip())
+        if "uncompress" in tool and tool["uncompress"]:
+            uncompress_cmd = tool["uncompress_cmd"]
+            uncompress_flags = ""
+            if "uncompress_flags" in tool:
+                uncompress_flags = tool["uncompress_flags"]
+                uncompress_flags = re.sub(r"\s+", " ", uncompress_flags.strip())
 
         tag_replace = None
         if "tag_replace" in tool:
@@ -73,61 +74,80 @@ def main():
         remove_v = False
         if "remove_v" in tool:
             remove_v = True
-            tag_replace = ['v', '']
+            tag_replace = ["v", ""]
 
-        releases = 'latest'
-        if 'releases' in tool:
-            releases = tool['releases']
-        url, tag, filename = get_url(user, repo, file_pattern, tag_replace=tag_replace, releases=releases)
+        releases = "latest"
+        if "releases" in tool:
+            releases = tool["releases"]
+        url, tag, filename = get_url(
+            user, repo, file_pattern, tag_replace=tag_replace, releases=releases
+        )
         if not url or not tag or not filename:
             continue
         if dry_run:
             urls.append(url)
             continue
 
-        print('Downloading from... {0}'.format(url))
-        full_filename = output + '/' + filename
-        subprocess.check_output(['wget', '-q', '-P', output, url])
+        print("Downloading from... {0}".format(url))
+        full_filename = output + "/" + filename
+        subprocess.check_output(["wget", "-q", "-P", output, url])
         if download_only:
             files.append(filename)
             continue
 
         os.chdir(output)
-        if 'uncompress' in tool and tool['uncompress']:
+        if "uncompress" in tool and tool["uncompress"]:
             if uncompress_flags:
-                subprocess.check_output([uncompress_cmd, uncompress_flags, full_filename])
+                subprocess.check_output(
+                    [uncompress_cmd, uncompress_flags, full_filename]
+                )
             else:
                 subprocess.check_output([uncompress_cmd, full_filename])
-            subprocess.check_output(['rm', full_filename])
-        if 'bin_dir_pattern' in tool:
-            bin_path = tool['bin_dir_pattern'].replace('###',tag)
-            full_bin_path = output + '/' + bin_path
-            if 'copy_to_bin' not in tool:
-                paths_to_append.append('export PATH='+full_bin_path+':$PATH')
+            subprocess.check_output(["rm", full_filename])
+        if "bin_dir_pattern" in tool:
+            bin_path = tool["bin_dir_pattern"].replace("###", tag)
+            full_bin_path = output + "/" + bin_path
+            if "copy_to_bin" not in tool:
+                paths_to_append.append("export PATH=" + full_bin_path + ":$PATH")
             else:
-                if 'copy_source_name' in tool:
-                    output_file_path = bin_directory + '/' + tool_name
-                    subprocess.check_output(['cp', full_bin_path + '/' + tool['copy_source_name'], output_file_path])
-                    if 'chmod' in tool:
-                        subprocess.check_output(['chmod', tool['chmod'], output_file_path])
+                if "copy_source_name" in tool:
+                    output_file_path = bin_directory + "/" + tool_name
+                    subprocess.check_output(
+                        [
+                            "cp",
+                            full_bin_path + "/" + tool["copy_source_name"],
+                            output_file_path,
+                        ]
+                    )
+                    if "chmod" in tool:
+                        subprocess.check_output(
+                            ["chmod", tool["chmod"], output_file_path]
+                        )
                 else:
-                    subprocess.check_output(['cp', full_bin_path + '/' + tool_name, bin_directory])
-                    if 'chmod' in tool:
-                        subprocess.check_output(['chmod', tool['chmod'], bin_directory])
+                    subprocess.check_output(
+                        ["cp", full_bin_path + "/" + tool_name, bin_directory]
+                    )
+                    if "chmod" in tool:
+                        subprocess.check_output(["chmod", tool["chmod"], bin_directory])
 
     if dry_run:
-        print('\nDry run completed. The URLs that would be downloaded are listed below:')
-        print('\n'.join(urls))
+        print(
+            "\nDry run completed. The URLs that would be downloaded are listed below:"
+        )
+        print("\n".join(urls))
     elif download_only:
-        print('\nThe list of files downloaded are printed below:')
-        print('\n'.join(files))
+        print("\nThe list of files downloaded are printed below:")
+        print("\n".join(files))
     elif not args.list:
-        print('All files downloaded and the below statements have been added to '+paths)
-        paths_to_append = '\n'.join(paths_to_append)
-        paths_to_append = '\n'+paths_to_append
+        print(
+            "All files downloaded and the below statements have been added to " + paths
+        )
+        paths_to_append = "\n".join(paths_to_append)
+        paths_to_append = "\n" + paths_to_append
         print(paths_to_append)
         with open(paths, "a") as paths_file:
             paths_file.write(paths_to_append)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/autodl/url.py
+++ src/autodl/url.py
@@ -2,7 +2,8 @@ import json
 import subprocess
 import sys
 
-def get_url(user, repo, file_pattern, tag_replace=None, releases='latest'):
+
+def get_url(user, repo, file_pattern, tag_replace=None, releases="latest"):
     if not user:
         print_error_valid("user")
     if not repo:
@@ -10,11 +11,13 @@ def get_url(user, repo, file_pattern, tag_replace=None, releases='latest'):
     if not file_pattern:
         print_error_valid("file_pattern")
 
-    result_str = ''
-    prepare_url = "https://api.github.com/repos/{0}/{1}/releases/{2}".format(user, repo, releases)
+    result_str = ""
+    prepare_url = "https://api.github.com/repos/{0}/{1}/releases/{2}".format(
+        user, repo, releases
+    )
     result = subprocess.check_output(["curl", "-s", prepare_url])
     if sys.version_info.major == 3:
-        result_str = result.decode('utf-8')
+        result_str = result.decode("utf-8")
     elif sys.version_info.major == 2:
         result_str = result
 
@@ -23,14 +26,16 @@ def get_url(user, repo, file_pattern, tag_replace=None, releases='latest'):
         sys.exit(2)
     # result_str = result.stdout.decode('utf-8')
     val = json.loads(result_str)
-    if not val or 'tag_name' not in val:
+    if not val or "tag_name" not in val:
         print("ERROR: tag_name not found in repo. please check the url...")
-        return '', '', ''
+        return "", "", ""
 
     tag_name = val["tag_name"]
     file_tag = tag_name
     if tag_replace:
         file_tag = tag_name.replace(tag_replace[0], tag_replace[1])
     file_pattern = file_pattern.replace("###", file_tag)
-    download_url = "https://github.com/{0}/{1}/releases/download/{2}/{3}".format(user, repo, tag_name, file_pattern)
+    download_url = "https://github.com/{0}/{1}/releases/download/{2}/{3}".format(
+        user, repo, tag_name, file_pattern
+    )
     return download_url, file_tag, file_pattern
